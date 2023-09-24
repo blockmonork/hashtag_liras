634036d0d615f5c321e9b667cdb99e0909973efa
blockmonork <blockmonork@gmail.com>
2023-09-24 15:35:33 -0300

    ta

--- teste.py
+++
@@ -1,9 +0,0 @@
-def idade(num=0):
-    hello(str(num) + ' anos')
-
-def hello(nome=""):
-    x = "" if nome=="" else ", " + nome
-    print(f"Hello {x}!")
-
-hello('fafm')
-idade('fabricio ' + str(45))
